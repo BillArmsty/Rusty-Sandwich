eb773bd8e0facc501e1da66fecee666064d7fe0e
Bill Armstrong <armsty9@gmail.com>
2023-07-11 13:39:41 +0300

    Added Uniswap v2 periphery

--- huff/src/RustySandwich.sol
+++ huff/src/RustySandwich.sol
@@ -5,6 +5,8 @@ pragma solidity ^0.8.13;
 import "v3-core/interfaces/IUniswapV3Pool.sol";
 import "v2-core/interfaces/IUniswapV2Pair.sol";
 import "../interfaces/IERC20.sol";
+import "v2-periphery/interfaces/IUniswapV2Router02.sol";
+
 
 
 contract RustySandwich  {
